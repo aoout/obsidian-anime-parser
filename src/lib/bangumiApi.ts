9189157f98cf3d2ee625496f2076b0a75e11f435
aoout <wuz66280@gmail.com>
2024-04-22 18:47:11 +0800

    refactor: bangumiApi

--- src/lib/bangumiApi.ts
+++ src/lib/bangumiApi.ts
@@ -1,10 +1,16 @@
 import { request2 } from "../utils/obsidianUtils";
 
+const BASEURL = "https://api.bgm.tv/";
+
+function caseInsensitiveEqual(str1, str2) {
+	return str1.toLowerCase() == str2.toLowerCase();
+}
+
 export default class bangumiApi {
 	static async search(name: string) {
 		// BUG: 不知道为什么，《梦想成为魔法少女》这部就是搜索不到。我听说过nsfw内容需要更多验证，不知道是不是因为这个。
 		const animes = await request2(
-			"https://api.bgm.tv/v0/search/subjects",
+			`${BASEURL}v0/search/subjects`,
 			"POST",
 			{},
 			{
@@ -14,21 +20,19 @@ export default class bangumiApi {
 				},
 			}
 		);
-		let result = animes["data"].find(
-			(anime) => anime["name_cn"].toLowerCase() == name.toLowerCase()
-		);
+		let result = animes["data"].find((anime) => caseInsensitiveEqual(anime["name_cn"], name));
 		if (!result) result = animes["data"][0];
 		return result;
 	}
 
 	static async getMetadata(id: number) {
-		const anime = await request2(`https://api.bgm.tv/v0/subjects/${id}`, "GET");
+		const anime = await request2(`${BASEURL}v0/subjects/${id}`, "GET");
 		return anime;
 	}
 
 	static async getEpisodes(id: number) {
 		const episodes = await request2(
-			"https://api.bgm.tv/v0/episodes",
+			`${BASEURL}v0/episodes`,
 			"GET",
 			{},
 			{
@@ -39,15 +43,15 @@ export default class bangumiApi {
 		return episodes["data"];
 	}
 
-	static async updateProgress(accessToken:string, id: number, progress: number) {
+	static async updateProgress(accessToken: string, id: number, progress: number) {
 		const episodes = await this.getEpisodes(id);
 		const episodeIds = episodes.map((episode) => episode["id"]);
 		return await request2(
-			`https://api.bgm.tv/v0/users/-/collections/${id}/episodes`,
+			`${BASEURL}v0/users/-/collections/${id}/episodes`,
 			"PATCH",
 			{
-				"Authorization": `Bearer ${accessToken}`,
-				"Content-Type": "application/json"
+				Authorization: `Bearer ${accessToken}`,
+				"Content-Type": "application/json",
 			},
 			{
 				episode_id: episodeIds.slice(0, progress),
