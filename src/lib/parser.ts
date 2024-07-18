d7c5aadd539f3009a52899b6f59576fb95dd5a42
aoout <wuz66280@gmail.com>
2024-07-18 21:48:05 +0800

    fix: fix parser

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -3,14 +3,14 @@ export function parseEpisode(fileNames: string[]): string[] {
 	const episodeMap = new Map();
 	fileNames.forEach(fileName => {
 		const match = fileName.match(
-			/(?![^ \[]])(第|未删减|_)?(?P<episode>\d+)(话|話|集|v2|先行版)?(?![^ \]])/
+			/(?![^ []])(第|未删减|_)?(?<episode>\d+)(话|話|集|v2|先行版)?(?![^ \]])/
 		);
 		if (match) {
 			episodeMap.set(fileName, Number(match[1]));
 		}
 	});
 	const episodes = Array.from(episodeMap.entries()).sort((a, b) => a[1] - b[1]).map(([fileName]) => fileName);
-	fileNames.forEach((fileName)=>fileNames.remove(fileName));
+	fileNames = [];
 	fileNames.push(...backup);
-	return episodes.reverse();
+	return episodes;
 }
