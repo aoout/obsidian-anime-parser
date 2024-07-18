5a4da131230f6afea90b676098ba9e278317228a
aoout <wuz66280@gmail.com>
2024-07-18 19:17:09 +0800

    feat: optimize the parser to adapt to the nth season animation (n>1)

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,8 +1,23 @@
 import * as path from "path";
 
-export function parseEpisode(fileNames: string[], start: number): string[] {
+export function parseEpisode(fileNames: string[]): string[] {
 	const backup = [...fileNames];
-	const indexs = Array.from({ length: fileNames.length }, (_, index) => index + start);
+	const commonSubstrings = findAllCommonSubstrings(fileNames);
+	const fileNameMap = new Map();
+	fileNames = fileNames.map((fileName) => {
+		let modifiedFileName = fileName;
+		commonSubstrings.forEach((subString) => {
+			modifiedFileName = modifiedFileName.replace(subString, "");
+		});
+		fileNameMap.set(modifiedFileName, fileName);
+		return modifiedFileName;
+	});
+	let maxEp = 1;
+	while (fileNames.some((fileName) => fileName.includes(maxEp.toString()))) {
+		maxEp++;
+	}
+	const startEp = maxEp - fileNames.length + 1;
+	const indexs = Array.from({ length: fileNames.length }, (_, index) => index + startEp);
 	const episodes: string[] = [];
 	for (let i = indexs.length - 1; i >= 0; i--) {
 		const index = indexs[i];
@@ -13,8 +28,34 @@ export function parseEpisode(fileNames: string[], start: number): string[] {
 			}))
 			.reduce((a, b) => (a.count > b.count ? a : b)).fileName;
 		fileNames.splice(fileNames.indexOf(episode), 1);
-		episodes.push(episode);
+		episodes.push(fileNameMap.get(episode));
 	}
 	fileNames.push(...backup);
 	return episodes.reverse();
+}
+
+function findAllCommonSubstrings(strings: string[]): string[] {
+	if (strings.length === 0) return [];
+
+	const shortest = strings.reduce((a, b) => (a.length <= b.length ? a : b));
+
+	const commonSubstrings = new Set<string>();
+
+	for (let length = shortest.length; length > 0; length--) {
+		for (let start = 0; start <= shortest.length - length; start++) {
+			const substring = shortest.substring(start, start + length);
+			if (strings.every((str) => str.includes(substring))) {
+				commonSubstrings.add(substring);
+			}
+		}
+	}
+
+	const result: string[] = [];
+	for (const substr of commonSubstrings) {
+		if (!Array.from(commonSubstrings).some((s) => s !== substr && s.includes(substr))) {
+			result.push(substr);
+		}
+	}
+
+	return result;
 }
